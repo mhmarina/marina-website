1727d7950c4c92531796b952ff84f285eb3c35f9
mhmarina <marinamagedsamuel@gmail.com>
2023-12-20 00:00:14 -0700

    add: deploy workflow

--- src/Pages/ArtPage.tsx
+++ src/Pages/ArtPage.tsx
@@ -1,13 +1,5 @@
 import Gallery from "../Components/Gallery";
 
-const messages: string[] = [
-  "not a lot going on at the moment.",
-  "rawr :3",
-  "this is art.",
-  "under construction",
-];
-const index = Math.floor(Math.random() * messages.length);
-
 function ArtPage() {
   return (
     <div style={{ marginTop: "135px" }}>
