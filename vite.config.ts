8e4a9c813f4f14fbe601a550e342ec6222ffc08c
mhmarina <marinamagedsamuel@gmail.com>
2024-01-19 00:54:30 -0700

    Optimized Images
    - used https://github.com/FatehAK/vite-plugin-image-optimizer to optimize images so they load faster

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/marina-website/',
-  plugins: [react()],
+  plugins: [react(), 
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 10,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 10,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 10,
+      },
+    }),]
 })
