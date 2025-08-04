8831162dfc63291615c113d2a6a684ef7d566e6b
Marina <140115877+mhmarina@users.noreply.github.com>
2025-08-04 14:11:33 -0700

    Remove node_modules
    why was i committing this ...

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -12,6 +12,10 @@ import { useEffect, useState } from "react";
 import HomePage from "../Pages/HomePage";
 import { supabase } from "../SupabaseClient";
 
+
+const hitsFile = "src/increment.txt"
+let fs = require('fs')
+
 function NavBar() {
   const headers = [
     "MEMEME",
@@ -32,57 +36,75 @@ function NavBar() {
 
   //fetch number of clicks on first render
   async function fetchHits() {
-    try {
-      const { data, error } = await supabase
-        .from("hits")
-        .select("num_hits")
-        .eq("id", 1);
 
-      if (error) {
-        console.log("ERROR FETCHING HITS");
-        setNumHits(0);
-      } else {
-        const initHits = data ? Number(data[0].num_hits) : -1;
-        setNumHits(initHits);
-        console.log("number of hits on first render: " + initHits);
-      }
-    } catch (error) {
-      console.error("An error occurred while fetching hits:", error);
-      setNumHits(0);
-    }
+    // *** Supabase:
+    // try {
+    //   const { data, error } = await supabase
+    //     .from("hits")
+    //     .select("num_hits")
+    //     .eq("id", 1);
+
+    //   if (error) {
+    //     console.log("ERROR FETCHING HITS");
+    //     setNumHits(0);
+    //   } else {
+    //     const initHits = data ? Number(data[0].num_hits) : -1;
+    //     setNumHits(initHits);
+    //     console.log("number of hits on first render: " + initHits);
+    //   }
+    // } catch (error) {
+    //   console.error("An error occurred while fetching hits:", error);
+    //   setNumHits(0);
+    // }
+
+    // *** .txt:
+    fetch(hitsFile)
+         .then((res) => {res.text()
+         .then((text) => {setNumHits(parseInt(text))})
+         })
   }
 
   useEffect(() => {
     fetchHits();
   }, []);
 
-  async function updateHits(newNumHits: number) {
-    try {
-      const { data, error } = await supabase
-        .from("hits")
-        .update({
-          num_hits: newNumHits,
-          last_updated: new Date().toISOString(),
-        })
-        .eq("id", 1)
-        .select();
-      if (error) {
-        console.error("Error updating hits:", error);
-      } else {
-        console.log("Hits updated successfully:", data[0]);
-      }
-    } catch (error) {
-      console.log("An error occured while updating hits: ", error);
-    }
-  }
+  // async function updateHits(newNumHits: number) {
+  //   // *** Code for supabase: 
+  //   // kind of unnecassary function actually
+  //   // try {
+  //   //   const { data, error } = await supabase
+  //   //     .from("hits")
+  //   //     .update({
+  //   //       num_hits: newNumHits,
+  //   //       last_updated: new Date().toISOString(),
+  //   //     })
+  //   //     .eq("id", 1)
+  //   //     .select();
+  //   //   if (error) {
+  //   //     console.error("Error updating hits:", error);
+  //   //   } else {
+  //   //     console.log("Hits updated successfully:", data[0]);
+  //   //   }
+  //   // } catch (error) {
+  //   //   console.log("An error occured while updating hits: ", error);
+  //   // }
+  // }
 
-  function onHitClick() {
-    setNumHits((prevNumHits) => {
-      const newNumHits = prevNumHits + 1;
-      updateHits(newNumHits);
-      console.log(newNumHits);
-      return newNumHits;
-    }); //store total number in a database so it persists after refreshing
+  async function onHitClick() {
+    // *** supabase:
+    // setNumHits((prevNumHits) => {
+    //   const newNumHits = prevNumHits + 1;
+    //   updateHits(newNumHits);
+    //   console.log(newNumHits);
+    //   return newNumHits;
+    // }); //store total number in a database so it persists after refreshing
+
+    // *** .txt:
+    let hits = await fetch(hitsFile).then((res) => res.text().then((text) => parseInt(text)))
+    hits = hits + 1
+    setNumHits(hits)
+    // write to file:
+    fs.write(hitsFile, hits.toString())
   }
 
   useEffect(() => {
